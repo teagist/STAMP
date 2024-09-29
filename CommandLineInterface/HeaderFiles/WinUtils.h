e85378e59237855f21576a993ef3bb075decfc90
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-09-28 20:31:16 -0500

    Create WinUtils.h

---
+++ CommandLineInterface/HeaderFiles/WinUtils.h
@@ -0,0 +1,61 @@
+//****************************************************************************
+//	Header File for Windows Utils
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 5/15/2024
+//****************************************************************************
+
+#ifndef _WIN_UTILS_H
+#define _WIN_UTILS_H
+
+#include <iostream>
+#include <ctime>
+#include <limits>
+
+// Needed for sleep function
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#define OK 1
+#define FAIL 0
+#define INFO 2
+
+namespace WinUtils
+{
+	enum States
+	{
+		PASSING = 0,
+		PASSED,
+		FAILING,
+		FAILED
+	};
+	
+	enum MessageLabels
+	{
+		BAD = 0,  // Change to FAIL later when update removes #defines
+		INFOR,
+		SUCCESS,
+		NOTICE		
+	};
+	
+	void delayBoot(int bootSeconds);
+	void wait(int seconds);
+	void pauseScreen();
+	void clearScreen();
+	
+	void printHardwareDetails();
+	void printOSDetails();
+	
+	void printUIBanner();	
+	void printInvalidUIBanner();
+	
+	int getUserInput();
+	int getYesOrNo(std::string prompt);
+	
+	void printErrorMsg(const std::string msg);
+	void printSuccessMsg(const std::string msg);
+}
+
+#endif
