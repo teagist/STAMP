7f1ee375caec362fc1f9fe0eac71afd84f3a82cb
Houston Brown <80162431+teagist@users.noreply.github.com>
2026-06-02 10:04:29 -0500

    Adding comment header. -HDB

--- CommandLineInterface/Source Files/Tutor.cpp
+++ CommandLineInterface/Source Files/Tutor.cpp
@@ -1,3 +1,12 @@
+//****************************************************************************
+//	STAMP Command Line Interface.
+//
+//  Tutor User Definition File.
+//
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 5/15/2024
+//****************************************************************************
+
 #include "WinUtils.h"
 #include "Tutor.h"
 
