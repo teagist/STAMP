c5121118048b3287e53f573f5d057559956fbaee
Houston Brown <80162431+teagist@users.noreply.github.com>
2026-06-02 10:06:01 -0500

    Update Faculty.h

--- CommandLineInterface/HeaderFiles/Faculty.h
+++ CommandLineInterface/HeaderFiles/Faculty.h
@@ -1,3 +1,12 @@
+//****************************************************************************
+//	STAMP Command Line Interface.
+//
+//  Faculty User Declaration File.
+//
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 5/15/2024
+//****************************************************************************
+
 #ifndef _FACULTY_H
 #define _FACULTY_H
 
