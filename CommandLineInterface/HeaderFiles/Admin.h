2dc70169f9a7fc319a2fd060c517fb9e52eee6e0
Houston Brown <80162431+teagist@users.noreply.github.com>
2026-06-02 10:05:44 -0500

    Update Admin.h

--- CommandLineInterface/HeaderFiles/Admin.h
+++ CommandLineInterface/HeaderFiles/Admin.h
@@ -1,3 +1,12 @@
+//****************************************************************************
+//	STAMP Command Line Interface.
+//
+//  Administrator User Declaration File.
+//
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 5/15/2024
+//****************************************************************************
+
 #ifndef _ADMIN_H
 #define _ADMIN_H
 
