3da3c31665db9b36eea054c1625c2a50cb955ef5
Houston Brown <80162431+teagist@users.noreply.github.com>
2026-06-02 10:07:07 -0500

    Update Student.h

--- CommandLineInterface/HeaderFiles/Student.h
+++ CommandLineInterface/HeaderFiles/Student.h
@@ -1,3 +1,12 @@
+//****************************************************************************
+//	STAMP Command Line Interface.
+//
+//  Student User Declaration File.
+//
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 5/15/2024
+//****************************************************************************
+
 #ifndef _STUDENT_H
 #define _STUDENT_H
 
